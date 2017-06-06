183f1bd6ce6eaeae30b3aded62280c54306ef60e
vidyachandasekhar <vidyahari.vs@Gmail.com>
2017-06-05 23:46:51 -0400

    working on StreamFIlters

--- src/main/java/com/zipcodewilmington/streams/StreamFilter.java
+++ src/main/java/com/zipcodewilmington/streams/StreamFilter.java
@@ -5,6 +5,7 @@ import com.zipcodewilmington.streams.anthropoid.PersonFactory;
 import com.zipcodewilmington.streams.tools.RandomUtils;
 import com.zipcodewilmington.streams.tools.StringUtils;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -20,7 +21,9 @@ public class StreamFilter {
      * No arg constructor
      */ //TODO - construct person stream of 100 person objects; startingCharacter is a random capital letter
     public StreamFilter() {
-        this(Stream.empty(), null);
+        Character c = RandomUtils.createCharacter('A','Z');
+        startingCharacter = Character.toString(c);
+        personStream = Stream.generate(PersonFactory::createRandomPerson).filter(s -> s.getName().startsWith(Character.toString(c))).limit(100);
     }
 
     /**
@@ -28,7 +31,9 @@ public class StreamFilter {
      * @param startingCharacter - character to filter by
      */ //TODO
     public StreamFilter(Person[] people, Character startingCharacter) {
-        this(Stream.empty(), null);
+        this.startingCharacter = Character.toString(startingCharacter);
+        personStream = Arrays.stream(people) .filter(s -> s.getName().startsWith(this.startingCharacter ));
+
     }
 
     /**
@@ -36,7 +41,8 @@ public class StreamFilter {
      * @param startingCharacter - character to filter by
      */ //TODO
     public StreamFilter(List<Person> people, Character startingCharacter) {
-        this(Stream.empty(), null);
+        this.startingCharacter = Character.toString(startingCharacter);
+        personStream = people.stream() .filter(s -> s.getName().startsWith(this.startingCharacter ));
     }
 
 
